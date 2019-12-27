c26b32d9c0003b2fd08a6c5ec94a90eeebe44d44
Hunter Treadaway <huntertreadaway@Hunters-MBP.attlocal.net>
2019-12-27 17:36:58 -0500

    succesfully created 10M record csv and seeded to pg db

--- dbhelpers/postgres/csv/writeToCsv.js
+++ dbhelpers/postgres/csv/writeToCsv.js
@@ -1,6 +1,7 @@
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const createCsvStringifier = require('csv-writer').createObjectCsvStringifier;
 const fs = require('fs');
+const file = fs.createWriteStream('./dbhelpers/postgres/csv/postgresRecords.csv');
 const faker = require('faker');
 
 listingAdjectives = ['sunset roost', 'lux', 'perfect nest', 'private pool', 'lakefront', 'charming', 'family-friendly', 'secluded', 'beach front', 'relaxing', 'lake cabin', 'pine cone', 'stunning', 'gorgeous modern', 'estes mountain', 'river front', 'water front', 'charming', 'winter special', 'beautiful', 'magnificent', 'ocean view', 'mountain getaway', 'colonial', 'rustic', 'wondrous', 'perfect getaway', 'quiet', 'historical', 'modern', 'gulf front', 'relaxing river', 'summer breezes', 'enchanting rockwood', 'cowell beach', 'rustic country', 'resort', 'panoramic ocean view', 'executive mountain', 'executive beach', 'executive', 'comfortable and spacious', 'private', 'mountain lookout', 'scenic luxury', 'great escape', 'stone lodge', 'dream retreat', 'dream guesthouse', 'enchanting', 'luxury', 'lighthouse view', 'comfortable', 'bay front', 'perfect & affordable', 'beach', 'scenic', 'location, location!', 'delightful vacation', 'grand', 'large', 'beautiful contemporary', 'ocean block', 'elegant', 'breathtaking', 'new', 'riverside', 'beach bungalow', 'wild dunes', 'stargazer lodge', 'quaint & cozy', 'downtown', 'amazing views', 'centrally located', 'seaside', 'newly remodeled', 'family vacation spot', 'palm', 'luxury', 'dune', 'fine-private', 'winter views', 'summer views', 'fall views', 'spring views', 'restored', 'expansive', 'conveniently located', 'total remodel', 'hidden', 'chic', 'victorian', 'sandy pines', 'Norman Rockwell', 'stylish', 'greek', 'revival style', 'spacious', 'good value', 'at the water edge', 'designer', 'fun', 'sandcastle', 'sky ledge', 'alpine', 'free night!', 'summer beach', 'absolutely gorgeous', 'asian-inspired', 'autumn', 'wine country', 'classic', 'bayside', 'vacation', 'southwest', 'immaculate', 'island style', 'beautiful slope', 'upscale', 'vintage', 'picturesque', 'redwood retreat', 'tuscan', 'cliffside', 'foresty', 'artistic', 'paradise', 'jet luxury', 'skyline view', 'view from space', 'burning cielings', 'mirror room', 'posh', 'abstract', 'sex dungeon', 'castle', 'tiny', 'cage'];
@@ -20,67 +21,6 @@ years = [2019, 2020];
 
 dates = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
 
-let listings = [];
-generateListings = async () => {
-  try {
-    let k;
-    let obj;
-    for (let i = 0; i < 10000000; i++) {
-      obj = {};
-      let title = listingAdjectives[Math.floor(Math.random() * listingAdjectives.length)] + ' ' + listingStyles[Math.floor(Math.random() * listingStyles.length)] + ' ' + listingAmenities[Math.floor(Math.random() * listingStyles.length)] + ' ' + listingAmenities[Math.floor(Math.random() * listingStyles.length)];
-      obj.title = title.slice(0, 1).toUpperCase() + title.slice(1);
-      obj.venue_type = listingType[Math.floor(Math.random() * listingType.length)];
-      obj.bedrooms = Math.floor(Math.random() * 5 + 1);
-      obj.sleep_capacity = obj.bedrooms * 2 + Math.floor(Math.random() * 3 + 1);
-      obj.bathrooms = Math.floor(Math.random() * 3 + 1);
-      obj.square_feet = Math.floor(Math.random() * 600 + 1) * 10 + 1000;
-      obj.review_overview = listingReview[Math.floor(Math.random() * listingReview.length)];
-      obj.rating = Math.floor(Math.random() * 10) / 10 + 4;
-      obj.review_number = Math.floor(Math.random() * 200 + 15);
-      obj.owner = faker.name.findName();
-      obj.cleaning_fee = Math.floor(Math.random() * 100) + 10;
-      obj.state = faker.address.state();
-      obj.city = faker.address.city();
-      k = i + 1;
-      obj.pic = `https://c8.alamy.com/comp/EPF1YW/nun-with-handgun-isolated-on-white-EPF1YW.jpg`;
-      await listings.push(obj); // may not need this, tbd
-    }
-  }
-  catch (err) {
-    console.error(err)
-  }
-};
-generateListings();
-
-/* use this block when generating less than 2M records */
-// const csvWriter = createCsvWriter({
-//   path: './dbhelpers/postgres/csv/postgresRecords.csv',
-//   header: [
-//     {id: 'title', title: 'TITLE'},
-//     {id: 'venue_type', title: 'VENUE_TYPE'},
-//     {id: 'bedrooms', title: 'BEDROOMS'},
-//     {id: 'bathrooms', title: 'BATHROOMS'},
-//     {id: 'sleep_capacity', title: 'SLEEP_CAPACITY'},
-//     {id: 'square_feet', title: 'SQUARE_FEET'},
-//     {id: 'review_overview', title: 'REVIEW_OVERVIEW'},
-//     {id: 'rating', title: 'RATING'},
-//     {id: 'review_number', title: 'REVIEW_NUMBER'},
-//     {id: 'owner', title: 'OWNER'},
-//     {id: 'cleaning_fee', title: 'CLEANING_FEE'},
-//     {id: 'state', title: 'STATE'},
-//     {id: 'city', title: 'CITY'},
-//     {id: 'pic', title: 'PIC'},
-//   ]
-// });
-
-// csvWriter.writeRecords(listings)
-//   .then(() => {
-//     console.log('done writing to csv');
-//   })
-//   .catch((err) => {
-//     console.error(err)
-//   });
-/* --------------------------------------- */
 
 const csvStringifier = createCsvStringifier({
   header: [
@@ -100,3 +40,51 @@ const csvStringifier = createCsvStringifier({
     {id: 'pic', title: 'PIC'},
   ]
 })
+
+const writeToCsv = async () => {
+  let listings = [];
+  generateListings = () => {
+    let k;
+    let obj;
+    for (let i = 0; i < 1000000; i++) {
+      obj = {};
+      let title = listingAdjectives[Math.floor(Math.random() * listingAdjectives.length)] + ' ' + listingStyles[Math.floor(Math.random() * listingStyles.length)] + ' ' + listingAmenities[Math.floor(Math.random() * listingStyles.length)] + ' ' + listingAmenities[Math.floor(Math.random() * listingStyles.length)];
+      obj.title = title.slice(0, 1).toUpperCase() + title.slice(1);
+      obj.venue_type = listingType[Math.floor(Math.random() * listingType.length)];
+      obj.bedrooms = Math.floor(Math.random() * 5 + 1);
+      obj.sleep_capacity = obj.bedrooms * 2 + Math.floor(Math.random() * 3 + 1);
+      obj.bathrooms = Math.floor(Math.random() * 3 + 1);
+      obj.square_feet = Math.floor(Math.random() * 600 + 1) * 10 + 1000;
+      obj.review_overview = listingReview[Math.floor(Math.random() * listingReview.length)];
+      obj.rating = Math.floor(Math.random() * 10) / 10 + 4;
+      obj.review_number = Math.floor(Math.random() * 200 + 15);
+      obj.owner = faker.name.findName();
+      obj.cleaning_fee = Math.floor(Math.random() * 100) + 10;
+      obj.state = faker.address.state();
+      obj.city = faker.address.city();
+      k = i + 1;
+      obj.pic = `https://c8.alamy.com/comp/EPF1YW/nun-with-handgun-isolated-on-white-EPF1YW.jpg`;
+      listings.push(obj);
+      console.log(i);
+    }
+  };
+  await generateListings();
+  await file.write(csvStringifier.stringifyRecords(listings));
+}
+
+const writeAll = async () => {
+  await writeToCsv();
+  await writeToCsv();
+  await writeToCsv();
+  await writeToCsv();
+  await writeToCsv();
+  await writeToCsv();
+  await writeToCsv();
+  await writeToCsv();
+  await writeToCsv();
+  await writeToCsv();
+  console.log('all done');
+}
+
+file.write(csvStringifier.getHeaderString()); // writes the csv header row
+writeAll();
