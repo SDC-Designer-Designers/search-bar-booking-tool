0efbe581a54595aa36fd6817558ac1c59640beab
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2019-12-30 16:40:17 -0800

    commented out un-used legacy express routes

--- server/index.js
+++ server/index.js
@@ -20,20 +20,29 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/dates/:id', (req, res) => {
   // console.log(req.params);
-  BookingDate.findAll({where: {listing_id: req.params.id}}).then(results => res.status(200).send(results)).catch(err => res.status(404).send(err));
+  BookingDate.findAll({where: {listing_id: req.params.id}})
+    .then(results => res.status(200).send(results))
+    .catch(err => res.status(404).send(err));
 });
 
-app.put('/dates/check_in/:id', (req, res) => {
-  BookingDate.update({check_in: false, available: true}, {where: {check_in: true, listing_id: req.params.id}}).then(() => {
-    BookingDate.update({check_in: true, available: false}, {where: {date: req.body.date, listing_id: req.params.id}}).then(() => res.status(200).send(results)).catch(err => res.status(404).send(err));
-  });
-});
+// app.put('/dates/check_in/:id', (req, res) => {
+//   BookingDate.update({check_in: false, available: true}, {where: {check_in: true, listing_id: req.params.id}})
+//     .then(() => {
+//       BookingDate.update({check_in: true, available: false}, {where: {date: req.body.date, listing_id: req.params.id}})
+//         .then(() => res.status(200).send(results))
+//         .catch(err => res.status(404).send(err));
+//     });
+// });
 
-app.put('/dates/check_out/:id', (req, res) => {
-  BookingDate.update({check_out: false, available: true}, {where: {check_out: true, listing_id: req.params.id}}).then(() => {
-    BookingDate.update({check_out: true, available: false}, {where: {date: req.body.date, listing_id: req.params.id}}).then(() => res.status(200).send(results)).catch(err => res.status(404).send(err));
-  });
-});
+// app.put('/dates/check_out/:id', (req, res) => {
+//   BookingDate.update({check_out: false, available: true}, {where: {check_out: true, listing_id: req.params.id}})
+//     .then(() => {
+//       BookingDate.update({check_out: true, available: false}, {where: {date: req.body.date, listing_id: req.params.id}})
+//         .then(() => res.status(200).send(results))
+//         .catch(err => res.status(404).send(err));
+//     });
+// });
+
 // get by location
 app.get('/listings/search', (req, res) => {
   let results = [];
@@ -50,28 +59,43 @@ app.get('/listings/search', (req, res) => {
 });
 
 app.get('/mlistings/:id', (req, res) => {
-  Listing.findAll({where: {id: req.params.id}}).then(results => {
-    res.status(200).send(results);
-  }).catch(err => res.status(404).send(err));
+  Listing.findAll({where: {id: req.params.id}})
+    .then(results => {
+      res.status(200).send(results);
+    })
+    .catch(err => res.status(404).send(err));
 });
 
-app.get('/listings/search/:id', (req, res) => {
-  BookingDate.findAll({where: {date: req.body.check_in_date, available: true, listing_id: req.params.id}}).then(results => {
-    if (results.length) {
-      BookingDate.findAll({where: {date: req.body.check_out_date, available: true, listing_id: req.params.id}}).then(results1 => {
-        if (results1.length) {
-          Listings.findAll({limit: 10, where: {title: {
-            $like: '%' + req.body.query + '%'
-          }, }}).then(results => res.status(200).send(results)).catch(err => res.status(404).send(err));
-        } else {
-          res.status(404).send('No listings found for these dates');
-        }
-      });
-    } else {
-      res.status(404).send('No listings found for these dates');
-    }
-  });
+app.put('/mlistings/:id', (req, res) => {
+
 });
+
+app.post('/mlistings/:id', (req, res) => {
+
+});
+
+app.delete('/mlistings/:id', (req, res) => {
+
+});
+
+// app.get('/listings/search/:id', (req, res) => {
+//   BookingDate.findAll({where: {date: req.body.check_in_date, available: true, listing_id: req.params.id}})
+//     .then(results => {
+//       if (results.length) {
+//         BookingDate.findAll({where: {date: req.body.check_out_date, available: true, listing_id: req.params.id}})
+//           .then(results1 => {
+//             if (results1.length) {
+//               Listings.findAll({limit: 10, where: {title: {$like: '%' + req.body.query + '%'}, }})
+//               .then(results => res.status(200).send(results)).catch(err => res.status(404).send(err));
+//             } else {
+//               res.status(404).send('No listings found for these dates');
+//             }
+//           });
+//       } else {
+//         res.status(404).send('No listings found for these dates');
+//       }
+//     });
+// });
 
 app.listen(port, () => {
   console.log('App is listening on port', port);
