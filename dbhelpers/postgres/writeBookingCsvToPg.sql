2d0f985d6f0c42358c1a6b7bfceec37eddd9b3de
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2020-01-03 17:55:32 -0800

    file clean up, forgot to switch branches and created a (hopefully) working index on PG database

--- dbhelpers/postgres/writeBookingCsvToPg.sql
+++ dbhelpers/postgres/writeBookingCsvToPg.sql
@@ -3,3 +3,5 @@
 COPY bookingdate
 FROM '/Users/huntertreadaway/code_projects/hack_reactor/hrla33/SDC/search-bar-booking-tool/dbhelpers/csv/bookingRecords.csv'
 DELIMITER ',' CSV HEADER;
+
+CREATE INDEX listing_id_index ON bookingdate (listing_id);
--- dbhelpers/postgres/writeListingCsvToPg.sql
+++ dbhelpers/postgres/writeListingCsvToPg.sql
@@ -3,3 +3,7 @@
 COPY listing
 FROM '/Users/huntertreadaway/code_projects/hack_reactor/hrla33/SDC/search-bar-booking-tool/dbhelpers/csv/listingRecords.csv'
 DELIMITER ',' CSV HEADER;
+
+CREATE INDEX listing_title_index ON listing (title varchar_pattern_ops);
+CREATE INDEX listing_state_index ON listing (state varchar_pattern_ops);
+CREATE INDEX listing_city_index ON listing (city varchar_pattern_ops);
