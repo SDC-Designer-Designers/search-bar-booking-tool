0a1e4bb2f2a06fb689377e101e57bbd7e8381625
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2020-01-07 10:47:08 -0800

    db connection update

--- dbhelpers/mongo/connection.js
+++ dbhelpers/mongo/connection.js
@@ -1,5 +1,10 @@
 var mongoose = require('mongoose');
-var mongoUri = "mongodb://localhost/sdcsearch";
+var userName = require('./creds').userName;
+var passWord = require('./creds').passWord;
+var dbip = require('./creds').dbip
+// var mongoUri = "mongodb://localhost/sdcsearch";
+// var mongoUri = `mongodb://${userName}:${passWord}@${dbip}:27017/sdcsearch`;
+var mongoUri = `mongodb://hunter:keepitsecret@34.212.34.103/sdcsearch`
 
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
---
+++ dbhelpers/mongo/creds.js
@@ -0,0 +1,7 @@
+const userName = "admin";
+const passWord = "admin123";
+const dbip = "34.219.151.2";
+
+module.exports = userName;
+module.exports = passWord;
+module.exports = dbip;
