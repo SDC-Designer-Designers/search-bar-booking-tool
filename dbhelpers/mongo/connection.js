dd23390a08c30ddcab5969ea9c08e4a9ecf2e8cc
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2020-01-08 10:55:02 -0800

    updated mongo connection

--- dbhelpers/mongo/connection.js
+++ dbhelpers/mongo/connection.js
@@ -1,10 +1,7 @@
 var mongoose = require('mongoose');
-var userName = require('./creds').userName;
-var passWord = require('./creds').passWord;
-var dbip = require('./creds').dbip
+require('dotenv').config();
 // var mongoUri = "mongodb://localhost/sdcsearch";
-// var mongoUri = `mongodb://${userName}:${passWord}@${dbip}:27017/sdcsearch`;
-var mongoUri = `mongodb://hunter:keepitsecret@34.212.34.103/sdcsearch`
+var mongoUri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/sdcsearch`;
 
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
--- dbhelpers/mongo/creds.js
+++
@@ -1,7 +0,0 @@
-const userName = "admin";
-const passWord = "admin123";
-const dbip = "34.219.151.2";
-
-module.exports = userName;
-module.exports = passWord;
-module.exports = dbip;
